308d06301ee1a1d7d0760d9a407a6fcea9e419b8
Josh Luo <josh.luo@gmail.com>
2013-09-24 11:13:06 -0700

    Add Merge Two Sorted Lists. Modified Add Two Numbers to use public ListNode class.

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -22,15 +22,6 @@
  * }
  */
 
-class ListNode {
-	int val;
-	ListNode next;
-	ListNode(int x) {
-		val = x;
-		next = null;
-	}
-}
-
 public class AddTwoNumbers {
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         // Start typing your Java solution below
