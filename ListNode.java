e5807404afd04860df5e5bda9f9525d74c0d8dd5
Josh Luo <josh.luo@gmail.com>
2013-09-24 11:12:02 -0700

    Add the helper class, ListNode, which is used throughout all the leetcode problems.

---
+++ ListNode.java
@@ -0,0 +1,16 @@
+/**
+ * Helper class. This is the definition for singly-linked list, which is used 
+ * throughout all the leetcode problems.
+ * 
+ * @author Josh Luo
+ *
+ */
+
+public class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x) {
+	    val = x;
+	    next = null;
+	}
+}
