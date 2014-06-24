6a69c8dde670a5e6e44e009019f816b280491e23
Josh Luo <josh.luo@gmail.com>
2014-06-23 23:03:47 -0700

    Commit Evaluate Reverse Polish Notation. Pretty easy question.

---
+++ EvaluateReversePolishNotation.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+/**
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation. http://en.wikipedia.org/wiki/Reverse_Polish_notation
+ * Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+ * 
+ * Some examples:
+ *   ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+ *	 ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+ * 
+ * @author joshluo
+ *
+ */
+public class EvaluateReversePolishNotation {
+	public int evalRPN(String[] tokens) {
+        Stack<Integer> values = new Stack<Integer>();
+        for(String token : tokens) {
+            if(token.equals("+")) {
+                values.push(values.pop() + values.pop());
+            } else if (token.equals("-")) {
+                int minused = values.pop();
+                values.push(values.pop() - minused);
+            } else if (token.equals("*")) {
+                values.push(values.pop() * values.pop());
+            } else if (token.equals("/")) {
+                int divided = values.pop();
+                values.push(values.pop() / divided);
+            } else {
+                values.push(Integer.parseInt(token));
+            }
+        }
+        return values.pop();
+    }
+}
