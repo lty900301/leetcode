fde10352f363ccb9fcb2e5a87b60e8ee4312d3fa
joshluo <josh.luo@gmail.com>
2015-01-03 12:29:32 -0800

    Better solution for Zig Zag Conversion.

--- ZigZagConversion.java
+++ ZigZagConversion.java
@@ -4,7 +4,9 @@
  * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to
  * display this pattern in a fixed font for better legibility)
  * 
- * P A H N A P L S I I G Y I R
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
  * 
  * And then read line by line: "PAHNAPLSIIGYIR"
  * 
@@ -19,7 +21,39 @@
  */
 
 public class ZigZagConversion {
+
+    /**
+     * Use this solution. Runtime: 576 ms <br>
+     * Look for the delta. For every row, column append the base current = col * delta + row first. And if not first row
+     * or last row, append the char in middle current = current + delta - 2 * i;
+     */
     public String convert(String s, int nRows) {
+        if (nRows < 1) {
+            return null;
+        }
+        if (nRows == 1) {
+            return s;
+        }
+        StringBuilder sb = new StringBuilder();
+        int delta = 2 * nRows - 2;
+        int columns = (s.length() - 1) / delta + 1;
+        for (int row = 0; row < nRows; row++) {
+            for (int column = 0; column < columns; column++) {
+                int current = row + column * delta;
+                if (current >= s.length()) {
+                    break;
+                }
+                sb.append(s.charAt(current));
+                current = current + delta - 2 * row;
+                if (row > 0 && row < nRows - 1 && current < s.length()) {
+                    sb.append(s.charAt(current));
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    public String convert1(String s, int nRows) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
         if (nRows == 0 || s.equals(""))
