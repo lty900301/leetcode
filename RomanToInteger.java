3d0fe910cf9d4b287893780f70dbe96bdbd26ec6
joshluo <josh.luo@gmail.com>
2015-01-04 00:06:46 -0800

    Improvements to Roman to Integer problem.

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Roman to Integer http://oj.leetcode.com/problems/roman-to-integer/
@@ -10,7 +11,36 @@ import java.util.HashMap;
  */
 
 public class RomanToInteger {
+
+    /**
+     * Cleaner and faster code.
+     */
     public int romanToInt(String s) {
+        final Map<Character, Integer> romanNumMap = new HashMap<Character, Integer>();
+        romanNumMap.put('I', 1);
+        romanNumMap.put('V', 5);
+        romanNumMap.put('X', 10);
+        romanNumMap.put('L', 50);
+        romanNumMap.put('C', 100);
+        romanNumMap.put('D', 500);
+        romanNumMap.put('M', 1000);
+
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int currentValue = romanNumMap.get(s.charAt(i));
+            result += currentValue;
+            if (i + 1 < s.length()) {
+                int nextValue = romanNumMap.get(s.charAt(i + 1));
+                if (nextValue > currentValue) { // combine
+                    result += (nextValue - 2 * currentValue);
+                    i++;
+                }
+            }
+        }
+        return result;
+    }
+
+    public int romanToInt2(String s) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
 
